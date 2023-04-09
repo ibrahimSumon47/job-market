c40693998725b322e05a16222953d0a632dac317
ibrahimSumon47 <ibrahimsumon005@gmail.com>
2023-04-09 23:48:44 +0600

    react router and tailwind daisyui installed

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-7xl'>Testing</h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import Header from './component/Header/Header'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Header></Header>
+  </React.StrictMode>,
+)
