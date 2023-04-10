067803ae03c463ddd0db99b1992551afceb312be
ibrahimSumon47 <ibrahimsumon005@gmail.com>
2023-04-11 03:07:55 +0600

    site responsive and some component added

--- src/component/FeaturedJobs/FeaturedJobs.jsx
+++ src/component/FeaturedJobs/FeaturedJobs.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import featuredJobsData from "../../../public/featuredJobsData.json";
+
+import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
     const [jobs, setJobs] = useState([]);
@@ -18,27 +19,8 @@ const FeaturedJobs = () => {
         </p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5 px-5 lg:px-80">
-        {featuredJobsData.map((featured) => (
-          <div key={featured.id} className="border rounded-lg p-5">
-            <img className="w-28" src={featured.companyLogo} alt="" />
-            <h4 className="font-bold py-1">{featured.jobTitle}</h4>
-            <p className="text-slate-400 pb-1">{featured.companyName}</p>
-            <div className="flex flex-row gap-10 pb-1">
-              <p className="border rounded-md w-28 h-auto text-center text-indigo-600 border-indigo-500 font-semibold">
-                {featured.remoteOrOnsite}
-              </p>
-              <p className="border rounded-md w-28 h-auto text-center text-indigo-600 border-indigo-500 font-semibold">
-                {featured.fulltimeOrParttime}
-              </p>
-            </div>
-            <div className="py-5 flex flex-row gap-10">
-              <p className="flex"><img src="../../../Images/Icons/location.png" alt="" />{featured.location}</p>
-              <p className="flex"><img src="../../../Images/Icons/Frame.png" alt="" /> Salary: {featured.salary}</p>
-            </div>
-            <button className="btn bg-indigo-400 text-slate-50 font-bold">
-              View Details
-            </button>
-          </div>
+        {jobs.map((job) => (
+          <Job key={job.id} job={job}></Job>
         ))}
       </div>
     </div>
---
+++ src/component/Job/Job.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import featuredJobsData from "../../../public/featuredJobsData.json";
+const Job = ({job}) => {
+    const {id, companyLogo, jobTitle, companyName, remoteOrOnsite, fulltimeOrParttime, location, salary} = job;
+    return (
+        <div>
+          <div key={id} className="border rounded-lg p-10">
+            <img className="w-28" src={companyLogo} alt="" />
+            <h4 className="font-bold py-1">{jobTitle}</h4>
+            <p className="text-slate-400 pb-1">{companyName}</p>
+            <div className="flex flex-row gap-10 mt-2">
+              <button className="border rounded-md w-28 h-auto text-center text-indigo-600 border-indigo-500 font-semibold">
+                {remoteOrOnsite}
+              </button>
+              <button className="border rounded-md w-28 h-auto text-center text-indigo-600 border-indigo-500 font-semibold">
+                {fulltimeOrParttime}
+              </button>
+            </div>
+            <div className="py-5 md:flex md:flex-row md:gap-10">
+              <div className='flex pb-5'>
+              <img src="../../../Images/Icons/location.png" alt="" />
+              <p>{location}</p>
+              </div>
+              <div className="flex">
+              <img className='h-6 w-6' src="../../../Images/Icons/Frame.png" alt="" />
+              <p>Salary:{salary}</p>
+              </div>
+            </div>
+            <button className="btn bg-indigo-400 text-slate-50 font-bold">
+              View Details
+            </button>
+          </div>
+      </div>
+    );
+};
+
+export default Job;
